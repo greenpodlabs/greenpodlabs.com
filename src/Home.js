cfab157518ed1d76f15d9803efefa1ed5486d9e5
scaffeinate <sudhar.ti@gmail.com>
2020-07-31 13:59:27 -0700

    Update layout

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Col, Container, Button } from 'react-bootstrap';
 import './Home.scss'
 
 const Home = () => (
     <Row>
-        <Col md={{ span: 4, offset: 0 }}>
+        <Col md={{ span: 3, offset: 0 }}>
             <img
                 src="/assets/images/sustainability.svg"
                 width="500"
@@ -13,10 +13,15 @@ const Home = () => (
                 alt="Greenpod Labs"
             />
         </Col>
-        <Col md={{ span: 6, offset: 1 }}>
-            <Container className="tagline">
+        <Col md={{ span: 6, offset: 2 }}>
+            <Container id="tagline">
                 <h1><b>Nature</b> Inspired Solutions to Minimize Food Waste.</h1>
                 <span>Our solution uses a unique composition of active compounds which creates a controlled environment which slows down the ripening rate and limits microbial growth.</span>
+                <Row>
+                    <Col md={{ span: 6, offset: 0 }}>
+                        <Button variant="primary" href="#">LEARN MORE</Button>{''}
+                    </Col>
+                </Row>
             </Container>
         </Col>
     </Row>
