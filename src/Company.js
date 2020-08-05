9d6e720b6662c35eec9862319c257862ccd9099e
scaffeinate <sudhar.ti@gmail.com>
2020-08-05 01:48:52 -0700

    Add icons

--- src/Company.js
+++ src/Company.js
@@ -5,53 +5,55 @@ import './Company.scss'
 const Company = () => (
     <Row id="company" className="top-margin">
         <Col xs={{ span: 10, offset: 1 }}>
+            <div className="text-center">
+                <h1 className="subheading">COMPANY</h1>
+            </div>
             <Row>
-                <Col>
-                    <div className="text-center">
-                        <h1 className="subheading">COMPANY</h1>
-                    </div>
-                </Col>
-            </Row>
-            <Row>
-                <Col md={5} className="mb-5 mb-md-0">
+                <Col md={{ span: 8, offset: 2 }}>
                     <span className="subtext">We are aligned towards United Nations’s Sustainable Development Goals of halving the food waste by <b>2030</b>. We work towards making this a reality.</span>
-                    <Button href="mailto: greenpodlabs@gmail.com">CONTACT US</Button>
-                </Col>
-                <Col md={{ span: 6, offset: 1}}>
-                    <Row>
-                        <Col lg={4} xs={6}>
-                            <Card className="shadow rounded mb-3">
-                                <Card.Body>Growth Mindset</Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} xs={6}>
-                            <Card className="shadow rounded mb-3">
-                                <Card.Body>Sustainability</Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} xs={6}>
-                            <Card className="shadow rounded mb-3">
-                                <Card.Body>First Principles Thinking</Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} xs={6}>
-                            <Card className="shadow rounded mb-3">
-                                <Card.Body>Customer Centered</Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} xs={6}>
-                            <Card className="shadow rounded mb-3">
-                                <Card.Body>Innovation with Agility</Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} xs={6}>
-                            <Card className="shadow rounded mb-3">
-                                <Card.Body>Integrity with Transparency</Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
                 </Col>
             </Row>
+            <Row className="mt-5 mt-xs-2">
+                <Col md={4} sm={12} className="mt-4">
+                    <Card className="shadow rounded" >
+                        <Card.Img variant="top" src="/assets/images/growth-mindset.png" fluid />
+                        <Card.Body>Growth Mindset</Card.Body>
+                    </Card>
+                </Col>
+                <Col md={4} sm={12} className="mt-4">
+                    <Card className="shadow rounded">
+                        <Card.Img variant="top" src="/assets/images/sustainability.png" fluid />
+                        <Card.Body>Sustainability</Card.Body>
+                    </Card>
+                </Col>
+                <Col md={4} sm={12} className="mt-4">
+                    <Card className="shadow rounded">
+                        <Card.Img variant="top" src="/assets/images/first-principle-thinking.png" fluid />
+                        <Card.Body>First Principles Thinking</Card.Body>
+                    </Card>
+                </Col>
+                <Col md={4} sm={12} className="mt-4">
+                    <Card className="shadow rounded">
+                        <Card.Img variant="top" src="/assets/images/customer-centered.png" fluid />
+                        <Card.Body>Customer Centered</Card.Body>
+                    </Card>
+                </Col>
+                <Col md={4} sm={12} className="mt-4">
+                    <Card className="shadow rounded">
+                        <Card.Img variant="top" src="/assets/images/innovation-with-agility.png" fluid />
+                        <Card.Body>Innovation with Agility</Card.Body>
+                    </Card>
+                </Col>
+                <Col md={4} sm={12} className="mt-4">
+                    <Card className="shadow rounded">
+                        <Card.Img variant="top" src="/assets/images/transparency.png" fluid />
+                        <Card.Body>Integrity with Transparency</Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <div className="text-center">
+                <Button href="mailto: greenpodlabs@gmail.com">CONTACT US</Button>
+            </div>
         </Col>
     </Row>
 );
